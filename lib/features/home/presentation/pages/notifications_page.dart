0d1811a3a4bbe941b3bced9543ed2be93b5cbe3b
Programmerhiman <bobberthegoodguy@gmail.com>
2026-08-03 16:32:38 +0530

    feat(notifications): initialize state and import dependencies

---
+++ lib/features/home/presentation/pages/notifications_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:go_router/go_router.dart';
+import '../../../../core/constants/app_colors.dart';
+import '../../../../core/constants/app_spacing.dart';
+import '../../../../core/constants/app_text_styles.dart';
+import '../../../../core/mock/mock_data.dart';
+import '../../data/models/notification_model.dart';
+
+class NotificationsPage extends StatefulWidget {
+  const NotificationsPage({super.key});
+
+  @override
+  State<NotificationsPage> createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  late List<NotificationModel> _notifs;
+  String _selectedCategory = 'All';
+
+  @override
+  void initState() {
+    super.initState();
+    _notifs = List.from(MockData.notifications);
+  }
+}
